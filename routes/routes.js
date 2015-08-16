ca53e771c41d3080535b06987199694ed36470ef
Hannu-Pekka Heinajarvi <hannupekka@gmail.com>
2015-08-16 21:15:47 +0300

    Comments.

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,6 @@
 // Require modules.
 var Joi = require('joi');
 var Bill = require('../models/bill.js');
-var _merge = require('lodash.merge');
 
 // Schema for payments.
 var schema = Joi.array().items(Joi.object().keys({
@@ -16,14 +15,22 @@ module.exports = [{
     path: '/bill',
     config: {
         validate: {
+            // Validate payload against schema.
             payload: {
                 data: schema,
             }
         },
+        // Handler.
         handler: function (request, reply) {
+            // Create new bill.
             Bill.create({payments: request.payload.data}, function(error, bill) {
-                if(error) { reply(error).code(500); return; }
+                // Return error code.
+                if(error) {
+                    reply(error).code(500);
+                    return;
+                }
 
+                // Reply with created bill data.
                 reply(bill).code(200);
             });
         }
@@ -34,24 +41,46 @@ module.exports = [{
     path: '/bill/{bid}',
     config: {
         validate: {
+            // Validate query param.
             params: {
                 bid: Joi.string().guid()
             },
+            // Validate payload against schema.
             payload: {
                 data: schema,
             }
         },
+        // Handler.
         handler: function (request, reply) {
             var bid = request.params.bid;
-            Bill.findOne({bid: bid}, function (error, bill) {
-                if(!bill) { reply(error).code(404); return;}
-                if(error) { reply(error).code(500); return;}
 
-                var updated = _merge(bill, request.payload.data);
-                updated.payments = request.payload.data;
-                updated.save(function (error) {
-                    if(error) { reply(error).code(500); return;}
-                    reply(updated).code(200);
+            // Find bill with matching bid.
+            Bill.findOne({bid: bid}, function (error, bill) {
+                // No matching bill found.
+                if(!bill) {
+                    reply(error).code(404);
+                    return;
+                }
+
+                // Return error code.
+                if(error) {
+                    reply(error).code(500);
+                    return;
+                }
+
+                // Update found bill payments.
+                bill.payments = request.payload.data;
+
+                // Save updated bill.
+                bill.save(function (error) {
+                    // Return error code.
+                    if(error) {
+                        reply(error).code(500);
+                        return;
+                    }
+
+                    // Reply with updated data.
+                    reply(bill).code(200);
                 });
             });
         }
@@ -62,17 +91,38 @@ module.exports = [{
     path: '/bill/{bid}',
     config: {
         validate: {
+            // Validate query param.
             params: {
                 bid: Joi.string().guid()
             }
         },
+        // Handler.
         handler: function (request, reply) {
             var bid = request.params.bid;
+
+            // Find bill with matching bid.
             Bill.findOne({bid: bid}, function (error, bill) {
-                if(!bill) { reply(error).code(404); return;}
-                if(error) { reply(error).code(500); return;}
+                // No matching bill found.
+                if(!bill) {
+                    reply(error).code(404);
+                    return;
+                }
+
+                // Return error code.
+                if(error) {
+                    reply(error).code(500);
+                    return;
+                }
+
+                // Remove bill.
                 bill.remove(function(error) {
-                    if(error) { reply(error).code(500); }
+                    // Return error code.
+                    if(error) {
+                        reply(error).code(500);
+                        return;
+                    }
+
+                    // Reply with OK.
                     reply().code(200);
                 });
             });
@@ -84,15 +134,30 @@ module.exports = [{
     path: '/bill/{bid}',
     config: {
         validate: {
+            // Validate query param.
             params: {
                 bid: Joi.string().guid()
             }
         },
+        // Handler.
         handler: function (request, reply) {
             var bid = request.params.bid;
+
+            // Find bill with matching bid.
             Bill.findOne({bid: bid}, function (error, bill) {
-                if(!bill) { reply(error).code(404); return;}
-                if(error) { reply(error).code(500); return;}
+                // No matching bill found.
+                if(!bill) {
+                    reply(error).code(404);
+                    return;
+                }
+
+                // Return error code.
+                if(error) {
+                    reply(error).code(500);
+                    return;
+                }
+
+                // Reply with payment data.
                 reply({data: bill.payments}).code(200);
             });
         }
